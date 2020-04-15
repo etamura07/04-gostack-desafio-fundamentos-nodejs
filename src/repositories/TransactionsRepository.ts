ce0d79e3d28f89b42991f7e681b9523013f8578d
emerson07 <emerson.tamura07@gmail.com>
2020-04-15 17:07:37 -0300

    jest ok

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionsWithBalanceDTO {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,44 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionsWithBalanceDTO {
+    const transactionsWithBalance = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return transactionsWithBalance;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeSum = this.transactions
+      .filter(({ type }) => type === 'income')
+      .reduce((sum: number, record) => {
+        return sum + Number(record.value);
+      }, 0);
+
+    const outcomeSum = this.transactions
+      .filter(({ type }) => type === 'outcome')
+      .reduce((sum: number, record) => {
+        return sum + Number(record.value);
+      }, 0);
+
+    const total = incomeSum - outcomeSum;
+
+    const balance = {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
